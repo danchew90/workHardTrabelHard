10ba34542d7be284a9396ff9fee6c14cf246f4d4
JongWon <danchew90@gmail.com>
2023-10-26 16:21:10 +0900

    Touchable

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { theme } from "./color";
+import { useState } from "react";
 
 export default function App() {
+  const [nowState, setNowState] = useState("W");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity
+          onPress={() => {
+            setNowState("W");
+          }}
+        >
+          <Text style={{ ...styles.btnText, color: nowState === "W" ? "#fff" : theme.gray }}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            setNowState("T");
+          }}
+        >
+          <Text style={{ ...styles.btnText, color: nowState === "T" ? "#fff" : theme.gray }}>Travel</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +31,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    flexDirection: "row",
+    marginTop: 100,
+    justifyContent: "space-between",
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: "600",
   },
 });
